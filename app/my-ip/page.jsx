bb7a69902488338e1704a83a4f57b7f270475417
Baelhj <syamo.imoo31@gmail.com>
2024-10-12 22:47:46 +0100

    fixed () on json

--- app/my-ip/page.jsx
+++ app/my-ip/page.jsx
@@ -22,7 +22,7 @@ const MyIp = () => {
     const fetchIPData = async () => {
     try {
       const response = await fetch("https://get.geojs.io/v1/ip/geo.json");
-      const data = await response.json;
+      const data = await response.json();
       setIpData(data)
     } catch (error) {
       console.error("Error fetching IP data:", error);
